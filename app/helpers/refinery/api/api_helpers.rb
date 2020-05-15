d06c4ad4fad2321b8d43f984404fbe9d20784a35
Stayman <stayman.hou@gmail.com>
2020-05-15 14:14:55 -0400

    blog post show categories instead of category ids

--- app/helpers/refinery/api/api_helpers.rb
+++ app/helpers/refinery/api/api_helpers.rb
@@ -46,7 +46,7 @@ module Refinery
       @@blog_post_attributes = [
         :id, :title, :body, :custom_teaser, :tag_list,
         :draft, :published_at, :custom_url, :user_id, :username, :browser_title,
-        :meta_description, :source_url, :source_url_title, category_ids: []
+        :meta_description, :source_url, :source_url_title, :categories
       ]
 
       @@inquiries_inquiry_attributes = [
